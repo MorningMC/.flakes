e9abe0361257a5d5b25307785c9ec81c395eb873
MorningMC <github@momc.qzz.io>
2026-06-05 19:14:50 +0800

    refactor: change host adventurers-server hardware settings
    The disk layout has changed due to system reinstall. This commit updates
    the new host hardware layout to match the current setup.

--- nixos/hosts/adventurers-server/hardware.nix
+++ nixos/hosts/adventurers-server/hardware.nix
@@ -8,7 +8,7 @@
 		extraModulePackages = [ ];
 
 		# Specify the swap device used for hibernation
-		resumeDevice = "/dev/disk/by-uuid/f6655e33-f593-4c1f-b173-b4acce0fe07a";
+		resumeDevice = "/dev/disk/by-uuid/04e4dd4b-f966-4a2f-a779-ff4bb65309c7";
 	};
 
 	# Enable hibernation
@@ -17,12 +17,12 @@
 	# Declare mount points
 	fileSystems = {
 		"/" = {
-			device = "/dev/disk/by-uuid/2274497c-9641-4913-b7ab-c1b443bae1f2";
+			device = "/dev/disk/by-uuid/151296a2-5a83-47ea-b811-cad97d6455ad";
 			fsType = "ext4";
 		};
 
 		"/boot" = {
-			device = "/dev/disk/by-uuid/7B47-D19E";
+			device = "/dev/disk/by-uuid/A39C-F21F";
 			fsType = "vfat";
 			options = [ "fmask=0077" "dmask=0077" ];
 		};
@@ -30,7 +30,7 @@
 
 	# Declare swap devices mounted
 	swapDevices = [
-		{ device = "/dev/disk/by-uuid/f6655e33-f593-4c1f-b173-b4acce0fe07a"; }
+		{ device = "/dev/disk/by-uuid/04e4dd4b-f966-4a2f-a779-ff4bb65309c7"; }
 	];
 
 	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
