7691befaf26c76601bdc6233d375ee75ccd9ef01
MorningMC <github@momc.qzz.io>
2026-06-21 11:17:18 +0800

    revert: refactor(networking): change interface metric on host adventurers-server
    This reverts commit f14a0f80bee4763ba4a7fa5dd653fe5f88ee3ecd.
    
    Changing route metric of network interfaces does not exhibit a noticable
    increase in overall network speed.

--- nixos/hosts/adventurers-server/default.nix
+++ nixos/hosts/adventurers-server/default.nix
@@ -10,23 +10,6 @@
 	# Define the hostname of the machine
 	networking.hostName = "adventurers-server";
 
-	# Prefer WiFi connection over Ethernet as it has higher speed
-	networking.networkmanager.settings = let
-		# Utility function to set a specific network interface's metric
-		setInterfaceMetric = interface: metric: {
-			match-device = "interface-name:" + interface;
-			"ipv4.route-metric" = metric;
-			"ipv6.route-metric" = metric;
-		};
-	in
-	{
-		# Set WiFi interface metric to 100
-		connection-wifi = setInterfaceMetric "wlo1" 100;
-
-		# Set Ethernet interface metric to 600
-		connection-ethernet = setInterfaceMetric "eno2" 600;
-	};
-
 	# Specify time zone.
 	time.timeZone = "Asia/Shanghai";
 
