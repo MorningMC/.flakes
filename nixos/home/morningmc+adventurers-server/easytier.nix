0f2e28683777f7aff84644a19f0ab2700e283026
MorningMC <github@momc.qzz.io>
2026-07-27 14:20:29 +0800

    refactor(nixos): expand easytier peer node list

--- nixos/home/morningmc+adventurers-server/easytier.nix
+++ nixos/home/morningmc+adventurers-server/easytier.nix
@@ -3,31 +3,47 @@
 	age.secrets.morningmc-easytier-adventurers.file = ./_secrets/easytier-adventurers.env.age;
 
 	# Enable EasyTier
-	services.easytier = {
-		enable = true;
+	services.easytier.enable = true;
 
-		# Define adventurers instance configurations
-		instances.adventurers = {
-			# Include network secrets
-			environmentFiles = [ config.age.secrets.morningmc-easytier-adventurers.path ];
+	# Define adventurers instance configurations
+	services.easytier.instances.adventurers = {
+		# Include network secrets
+		environmentFiles = [ config.age.secrets.morningmc-easytier-adventurers.path ];
 
-			# Specify IPv4 address allocated
-			settings.ipv4 = "10.144.144.10/24";
+		# Specify IPv4 address allocated
+		settings.ipv4 = "10.144.144.10/24";
 
-			# Specify peer nodes to connect on service start
-			settings.peers = [
-				"tcp://public.easytier.top:11010"
-				"tcp://adventurers.morningmc.qzz.io:11010"
-				"udp://adventurers.morningmc.qzz.io:11010"
-				"tcp://103.184.47.79:11010"
-				"tcp://c.oee.icu:60006"
-				"tcp://et.gbc.moe:11010"
-				"tcp://turn.nmg.629957.xyz:11010"
-				"tcp://weior.top:11010"
-				"udp://easytier.weiai.org.cn:11010"
-				"tcp://ros.scpsl.com.cn:11010"
-				"tcp://106.15.202.147:11010"
-			];
-		};
+		# Specify peer nodes to connect on service start
+		settings.peers = [
+			# Official public servers
+			"tcp://public.easytier.top:11010"
+			"tcp://easytier.public.kkrainbow.top:11010"
+
+			# Self-host server
+			"tcp://v4.adventurers.morningmc.qzz.io:11010"
+			"udp://v4.adventurers.morningmc.qzz.io:11010"
+
+			# Third-party servers (partially collected from https://ruixuan.online/uptime/easytier)
+			"tcp://easytier.weiai.org.cn:11010"
+			"tcp://103.184.47.79:11010"
+			"tcp://c.oee.icu:60006"
+			"tcp://et.gbc.moe:11010"
+			"tcp://turn.nmg.629957.xyz:11010"
+			"tcp://weior.top:11010"
+			"tcp://ros.scpsl.com.cn:11010"
+			"tcp://106.15.202.147:11010"
+			"tcp://225284.xyz:11010"
+			"udp://225284.xyz:11010"
+			"udp://us01.225284.xyz:11010"
+			"tcp://183.230.36.171:11010"
+			"wss://et.南梁.com"
+			"tcp://qic.南梁.com:11010"
+			"wss://et.chinokou.cn"
+			"tcp://et.sbgov.cn:11010"
+			"udp://47.120.54.91:11010"
+			"udp://et.basd1.de:22020"
+			"tcp://qwe.p8.ink:11010"
+			"tcp://37.221.197.17:11010"
+		];
 	};
 }
