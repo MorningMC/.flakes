bcf7f5608688bff211b3e680fbd96b82c85ee780
MorningMC <github@momc.qzz.io>
2026-08-29 17:40:30 +0800

    refactor(nixos): override minecraft-ttf to include all types and styles of font
    Unifont is removed as minecraft-ttf has already covered uniform fonts &
    it cannot be scaled above 16px.
    
    Refs: 70242db (chore(minecraft-ttf): update build logic to match
    upstream changes)

--- nixos/home/morningmc+morningmc-laptop/fonts.nix
+++ nixos/home/morningmc+morningmc-laptop/fonts.nix
@@ -21,9 +21,19 @@
 			vista-fonts
 
 			# Minecraft fonts
-			inputs.minecraft-ttf.packages.${stdenv.hostPlatform.system}.default
-			unifont # Minecraft uniform font
-			ark-pixel-font # Alternative pixel font
+			(inputs.minecraft-ttf.packages.${stdenv.hostPlatform.system}.default.override {
+				# Generate for all types of font
+				identifiers = [ "minecraft:default" "minecraft:alt" "minecraft:illageralt" "minecraft:uniform" ];
+
+				# Generate for all styles
+				styles = [ "regular" "bold" "italic" "bold_italic" ];
+
+				# Include printable characters on Unicode plane 0 in uniform font
+				unifont-chars = "0200-d7ff,e000-fffd";
+			})
+
+			# Alternative pixel fonts
+			ark-pixel-font
 		];
 	};
 }
